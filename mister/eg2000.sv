4e1f34c060ec4a2df62fe6a9bbac9ae4d0cca065
JasonCoombes-hr <74770401+JasonCoombes-hr@users.noreply.github.com>
2022-03-01 21:54:10 +0100

    Files

---
+++ mister/eg2000.sv
@@ -0,0 +1,449 @@
+//============================================================================
+//
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of the GNU General Public License as published by the Free
+//  Software Foundation; either version 2 of the License, or (at your option)
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful, but WITHOUT
+//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+//  more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+//============================================================================
+
+module emu
+(
+	//Master input clock
+	input         CLK_50M,
+
+	//Async reset from top-level module.
+	//Can be used as initial reset.
+	input         RESET,
+
+	//Must be passed to hps_io module
+	inout  [45:0] HPS_BUS,
+
+	//Base video clock. Usually equals to CLK_SYS.
+	output        CLK_VIDEO,
+
+	//Multiple resolutions are supported using different CE_PIXEL rates.
+	//Must be based on CLK_VIDEO
+	output        CE_PIXEL,
+
+	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
+	//if VIDEO_ARX[12] or VIDEO_ARY[12] is set then [11:0] contains scaled size instead of aspect ratio.
+	output [12:0] VIDEO_ARX,
+	output [12:0] VIDEO_ARY,
+
+	output  [7:0] VGA_R,
+	output  [7:0] VGA_G,
+	output  [7:0] VGA_B,
+	output        VGA_HS,
+	output        VGA_VS,
+	output        VGA_DE,    // = ~(VBlank | HBlank)
+	output        VGA_F1,
+	output [1:0]  VGA_SL,
+	output        VGA_SCALER, // Force VGA scaler
+
+	input  [11:0] HDMI_WIDTH,
+	input  [11:0] HDMI_HEIGHT,
+
+`ifdef USE_FB
+	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
+	// FB_FORMAT:
+	//    [2:0] : 011=8bpp(palette) 100=16bpp 101=24bpp 110=32bpp
+	//    [3]   : 0=16bits 565 1=16bits 1555
+	//    [4]   : 0=RGB  1=BGR (for 16/24/32 modes)
+	//
+	// FB_STRIDE either 0 (rounded to 256 bytes) or multiple of pixel size (in bytes)
+	output        FB_EN,
+	output  [4:0] FB_FORMAT,
+	output [11:0] FB_WIDTH,
+	output [11:0] FB_HEIGHT,
+	output [31:0] FB_BASE,
+	output [13:0] FB_STRIDE,
+	input         FB_VBL,
+	input         FB_LL,
+	output        FB_FORCE_BLANK,
+
+	// Palette control for 8bit modes.
+	// Ignored for other video modes.
+	output        FB_PAL_CLK,
+	output  [7:0] FB_PAL_ADDR,
+	output [23:0] FB_PAL_DOUT,
+	input  [23:0] FB_PAL_DIN,
+	output        FB_PAL_WR,
+`endif
+
+	output        LED_USER,  // 1 - ON, 0 - OFF.
+
+	// b[1]: 0 - LED status is system status OR'd with b[0]
+	//       1 - LED status is controled solely by b[0]
+	// hint: supply 2'b00 to let the system control the LED.
+	output  [1:0] LED_POWER,
+	output  [1:0] LED_DISK,
+
+	// I/O board button press simulation (active high)
+	// b[1]: user button
+	// b[0]: osd button
+	output  [1:0] BUTTONS,
+
+	input         CLK_AUDIO, // 24.576 MHz
+	output [15:0] AUDIO_L,
+	output [15:0] AUDIO_R,
+	output        AUDIO_S,   // 1 - signed audio samples, 0 - unsigned
+	output  [1:0] AUDIO_MIX, // 0 - no mix, 1 - 25%, 2 - 50%, 3 - 100% (mono)
+
+	//ADC
+	inout   [3:0] ADC_BUS,
+
+	//SD-SPI
+	output        SD_SCK,
+	output        SD_MOSI,
+	input         SD_MISO,
+	output        SD_CS,
+	input         SD_CD,
+
+`ifdef USE_DDRAM
+	//High latency DDR3 RAM interface
+	//Use for non-critical time purposes
+	output        DDRAM_CLK,
+	input         DDRAM_BUSY,
+	output  [7:0] DDRAM_BURSTCNT,
+	output [28:0] DDRAM_ADDR,
+	input  [63:0] DDRAM_DOUT,
+	input         DDRAM_DOUT_READY,
+	output        DDRAM_RD,
+	output [63:0] DDRAM_DIN,
+	output  [7:0] DDRAM_BE,
+	output        DDRAM_WE,
+`endif
+
+`ifdef USE_SDRAM
+	//SDRAM interface with lower latency
+	output        SDRAM_CLK,
+	output        SDRAM_CKE,
+	output [12:0] SDRAM_A,
+	output  [1:0] SDRAM_BA,
+	inout  [15:0] SDRAM_DQ,
+	output        SDRAM_DQML,
+	output        SDRAM_DQMH,
+	output        SDRAM_nCS,
+	output        SDRAM_nCAS,
+	output        SDRAM_nRAS,
+	output        SDRAM_nWE,
+`endif
+
+`ifdef DUAL_SDRAM
+	//Secondary SDRAM
+	input         SDRAM2_EN,
+	output        SDRAM2_CLK,
+	output [12:0] SDRAM2_A,
+	output  [1:0] SDRAM2_BA,
+	inout  [15:0] SDRAM2_DQ,
+	output        SDRAM2_nCS,
+	output        SDRAM2_nCAS,
+	output        SDRAM2_nRAS,
+	output        SDRAM2_nWE,
+`endif
+
+	input         UART_CTS,
+	output        UART_RTS,
+	input         UART_RXD,
+	output        UART_TXD,
+	output        UART_DTR,
+	input         UART_DSR,
+
+	// Open-drain User port.
+	// 0 - D+/RX
+	// 1 - D-/TX
+	// 2..6 - USR2..USR6
+	// Set USER_OUT to 1 to read from USER_IN.
+	input   [6:0] USER_IN,
+	output  [6:0] USER_OUT,
+
+	input         OSD_STATUS
+);
+
+///////// Default values for ports not used in this core /////////
+
+assign ADC_BUS  = 'Z;
+assign USER_OUT = '1;
+assign {UART_RTS, UART_TXD, UART_DTR} = 0;
+assign {SD_SCK, SD_MOSI, SD_CS} = 'Z;
+assign {SDRAM_DQ, SDRAM_A, SDRAM_BA, SDRAM_CLK, SDRAM_CKE, SDRAM_DQML, SDRAM_DQMH, SDRAM_nWE, SDRAM_nCAS, SDRAM_nRAS, SDRAM_nCS} = 'Z;
+assign {DDRAM_CLK, DDRAM_BURSTCNT, DDRAM_ADDR, DDRAM_DIN, DDRAM_BE, DDRAM_RD, DDRAM_WE} = '0;  
+
+assign VGA_SL = (scale[2]==1'b1)?2'd0:scale[1:0];
+
+assign VGA_F1 = 0;
+assign VGA_SCALER = 0;
+
+assign AUDIO_S = 0;
+assign AUDIO_MIX = 0;
+
+//assign LED_DISK = 0;
+assign LED_POWER = 0;
+assign BUTTONS = 0;
+
+
+//////////////////////////////////////////////////////////////////
+
+
+`include "build_id.v" 
+localparam CONF_STR = {
+	"eg2000;;",
+	"-;",
+	"F1,CAS,Load Cassette;",
+	"-;",
+	"O89,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
+    "OAC,Scandoubler Fx,None,CRT 25%,CRT 50%,CRT 75%,HQ2x;",
+	"OFG,Scale,Normal,V-Integer,Narrower HV-Integer,Wider HV-Integer;",
+    "-;",
+	"T0,Reset;",
+	"R0,Reset and close OSD;",
+	"V,v",`BUILD_DATE 
+};
+
+wire [21:0] gamma_bus;
+wire forced_scandoubler;
+wire  [1:0] buttons;
+wire [31:0] status;
+wire  [1:0] ps2;
+
+wire        ioctl_download;
+wire  [7:0] ioctl_index;
+wire        ioctl_wr;
+wire [24:0] ioctl_addr;
+wire  [7:0] ioctl_data;
+
+// PS2DIV : la mitad del divisor que necesitas para dividir el clk_sys que le das al hpio, para que te de entre 10Khz y 16Kzh
+hps_io #(.STRLEN($size(CONF_STR)>>3), .PS2DIV(2500)) hps_io
+(
+	.clk_sys		(clk_sys),
+	.HPS_BUS		(HPS_BUS),
+	.EXT_BUS		(),
+
+	.conf_str	(CONF_STR),
+	.forced_scandoubler(forced_scandoubler),
+
+	.buttons		(buttons),
+	.status		(status),
+//	.status_menumask({status[5]}),
+	.ps2_kbd_clk_out (ps2[0]),
+    .ps2_kbd_data_out(ps2[1]),
+
+	//ioctl
+	.ioctl_download(ioctl_download),
+	.ioctl_index(ioctl_index),
+	.ioctl_wr(ioctl_wr),
+	.ioctl_addr(ioctl_addr),
+	.ioctl_dout(ioctl_data)   
+  );
+
+///////////////////////   CLOCKS   ///////////////////////////////
+
+wire clk_sys;
+wire pll_locked;
+
+pll pll
+(
+	.refclk   (CLK_50M),
+	.rst      (0),
+	.outclk_0 (clk_sys),
+	.locked   (pll_locked)
+);
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+wire HBlank;
+wire HSync;
+wire VBlank;
+wire VSync;
+wire ce_pix;
+wire led;
+wire pixel;
+
+reg[5:0] rs;
+wire power = rs[5] & ~status[0] & ~RESET & ~buttons[1];
+always @(posedge clk_sys) if(!power) rs <= rs+1'd1;
+
+wire[3:0] color;
+
+// signals from loader
+wire loader_wr;		
+wire loader_download;
+wire [15:0] loader_addr;
+wire [7:0] loader_data;
+wire [15:0] execute_addr;
+wire execute_enable;
+wire loader_wait;
+
+wire trsram_wr;			// Writing loader data to ram 
+wire trsram_download;	// Download in progress (active high)
+wire [23:0] trsram_addr;
+wire [7:0] trsram_data;
+
+assign trsram_wr = loader_download ? loader_wr : ioctl_wr;
+assign trsram_download = loader_download ? loader_download : ioctl_index == 1 ? ioctl_download : 1'b0;
+assign trsram_addr = loader_download ? {8'b0, loader_addr} : {|ioctl_index,ioctl_addr};
+assign trsram_data = loader_download ? loader_data : ioctl_data;
+
+glue Glue
+(
+	.clock  (clk_sys),
+	//.reset	(reset),
+	.power  (power  ),
+	.hsync  (HSync),
+	.vsync  (VSync),
+	.hblank  (HBlank),
+	.vblank  (VBlank),
+
+	.ce_pix (ce_pix ),
+	.pixel  (pixel  ),
+	.color  (color  ),
+	.crtcDe (crtcDe_tmp),
+	.tape   (~tape_in),
+	.audio_l(AUDIO_L),
+	.audio_r(AUDIO_R),
+	.led    (led    ),
+	.ps2    (ps2    ),
+
+    .dn_clk	(clk_sys),
+    .dn_go (trsram_download),
+    .dn_wr (trsram_wr),
+    .dn_addr (trsram_addr),
+    .dn_data (trsram_data),
+    
+    .loader_download (loader_download),
+    .execute_addr (execute_addr),
+    .execute_enable (execute_enable)	
+);
+
+
+reg[17:0] palette[15:0];
+initial begin
+	palette[15] = 18'b111111_111111_111111; // FF FF FF // 16 // white
+	palette[14] = 18'b100110_001000_111111; // 98 20 FF //  8 // magenta
+	palette[13] = 18'b000111_110001_100011; // 1F C4 8C // 14 // turquise
+	palette[12] = 18'b100011_100011_100011; // 8C 8C 8C // 13 // grey
+	palette[11] = 18'b100010_011001_111111; // 8A 67 FF // 12 // violet
+	palette[10] = 18'b110001_010011_111111; // C7 4E FF // 15 // pink
+	palette[ 9] = 18'b101111_110111_111111; // BC DF FF //  9 // light blue
+	palette[ 8] = 18'b001011_010100_111111; // 2F 53 FF //  8 // blue
+	palette[ 7] = 18'b111010_111111_001001; // EA FF 27 // 11 // yellow/green
+	palette[ 6] = 18'b111010_011011_001010; // EB 6F 2B //  5 // orange
+	palette[ 5] = 18'b101010_111111_010010; // AB FF 4A //  2 // green
+	palette[ 4] = 18'b111111_111100_001111; // FF F2 3D //  4 // yellow
+	palette[ 3] = 18'b111010_111010_111010; // EA EA EA //  1 // light grey
+	palette[ 2] = 18'b110010_001001_010111; // CB 26 5E //  3 // red
+	palette[ 1] = 18'b011011_111111_111010; // 7C FF EA //  7 // cyan
+	palette[ 0] = 18'b010111_010111_010111; // 5E 5E 5E // 10 // dark grey
+end
+
+wire[17:0] rgbQ = pixel ? palette[color] : 1'd0;
+
+assign CLK_VIDEO = clk_sys;
+
+wire ce_pix_out;
+wire scandoubler = scale || forced_scandoubler;
+wire [2:0] scale = status[12:10];
+wire crtcDe_tmp;
+
+// YOM Sin Video_Mixer ni Video_Freak
+//assign CE_PIXEL = ce_pix;
+//assign VGA_R = {rgbQ[17:12],2'b0};
+//assign VGA_G = {rgbQ[11: 6],2'b0};
+//assign VGA_B = {rgbQ[ 5: 0],2'b0};
+//assign VGA_VS = VSync;
+//assign VGA_HS = HSync;
+//assign VGA_DE = crtcDe_tmp;//~(VBlank | HBlank);
+//
+//assign VIDEO_ARX = 4;
+//assign VIDEO_ARY = 3;
+
+// YOM Sin Video_Freak pero si con video_mixer
+//assign VIDEO_ARX = 4;
+//assign VIDEO_ARY = 3;
+//assign VGA_DE = crtcDe_tmp;
+
+video_mixer #(.LINE_LENGTH(640)) video_mixer
+(
+	.CLK_VIDEO		(CLK_VIDEO),
+	.CE_PIXEL		(CE_PIXEL),
+	.ce_pix			(ce_pix),
+	.scandoubler	(scandoubler),
+	.gamma_bus		(gamma_bus),
+	.HSync			(HSync),
+	.VSync			(VSync),
+	.HBlank			(HBlank),
+	.VBlank			(VBlank),
+
+	// video output signals
+	.VGA_R			(VGA_R), //output reg [7:0] VGA_R,
+	.VGA_G			(VGA_G), //output reg [7:0] VGA_G,
+	.VGA_B			(VGA_B), //output reg [7:0] VGA_B,
+	.VGA_VS			(VGA_VS), //output reg       VGA_VS,
+	.VGA_HS			(VGA_HS), //output reg       VGA_HS,		
+	.VGA_DE			(VGA_DE), //output reg       VGA_DE,
+		
+	.R         		({rgbQ[17:12],2'b0}),
+   .G         		({rgbQ[11: 6],2'b0}),
+   .B         		({rgbQ[ 5: 0],2'b0}),
+	.hq2x      		(scale[2])
+);
+
+
+wire [1:0] ar = status[9:8];
+
+video_freak video_freak
+(
+	.CLK_VIDEO		(CLK_VIDEO),
+	.CE_PIXEL		(CE_PIXEL),
+	.VGA_VS			(VGA_VS),
+	.HDMI_WIDTH		(HDMI_WIDTH), //input      [11:0] HDMI_WIDTH,
+	.HDMI_HEIGHT	(HDMI_HEIGHT), //input      [11:0] HDMI_HEIGHT,
+	.VIDEO_ARX		(VIDEO_ARX), //output reg [12:0] VIDEO_ARX,
+	.VIDEO_ARY		(VIDEO_ARY), //output reg [12:0] VIDEO_ARY,
+	
+	.VGA_DE_IN     (VGA_DE),
+	.VGA_DE        (),
+	.ARX           ((!ar) ? 12'd4 : (ar - 1'd1)),
+	.ARY           ((!ar) ? 12'd3 : 12'd0),
+	.CROP_SIZE     (0),
+	.CROP_OFF      (0),
+	.SCALE         (status[16:15])
+);
+
+
+
+
+//Mister TapeIn
+
+wire tape_in;
+wire tape_adc, tape_adc_act;
+
+assign tape_in = tape_adc_act & tape_adc;
+
+
+ltc2308_tape  tape
+(
+  .clk		(CLK_50M), 
+  .ADC_BUS	(ADC_BUS),
+  .dout		(tape_adc),
+  .active	(tape_adc_act)
+);
+
+assign LED_USER    = ~tape_in;
+
+
+
+endmodule
---
+++ src/glue.v
@@ -0,0 +1,467 @@
+//-------------------------------------------------------------------------------------------------
+module glue
+//-------------------------------------------------------------------------------------------------
+(
+	input  wire       clock,
+	//input  wire 	  reset,	
+	input  wire       power,
+
+	output wire       hsync,
+	output wire       vsync,
+`ifdef USE_CE_PIX
+	output wire       ce_pix,
+`endif	
+
+`ifdef USE_BLANK
+	output wire       hblank,
+	output wire       vblank,
+`endif	
+
+	output wire       pixel,
+	output wire[ 3:0] color,
+	output wire			crtcDe,
+
+	input  wire       tape,
+`ifdef USE_DAC
+	output wire       sound,
+`else
+   output wire [15:0] audio_l,
+   output wire [15:0] audio_r,
+`endif
+	input  wire[ 1:0] ps2,
+	output wire       led,
+`ifdef ZX1
+	output wire       boot,
+	output wire       ramWe,
+	inout  wire[ 7:0] ramDQ,
+	output wire[20:0] ramA,
+`elsif USE_BRAM
+	output wire       filler,
+`elsif USE_SDRAM
+	output wire       ramCk,
+	output wire       ramCe,
+	output wire       ramCs,
+	output wire       ramWe,
+	output wire       ramRas,
+	output wire       ramCas,
+	output wire[ 1:0] ramDqm,
+	inout  wire[15:0] ramDQ,
+	output wire[ 1:0] ramBA,
+	output wire[12:0] ramA,
+`endif
+
+    input            dn_clk,
+    input            dn_go,
+    input            dn_wr,
+    input [24:0]     dn_addr,
+    input [7:0]      dn_data,
+    
+    input            loader_download,
+    input [15:0]     execute_addr,
+    input            execute_enable	
+);
+//-------------------------------------------------------------------------------------------------
+
+reg[4:0] ce;
+always @(negedge clock) ce <= ce+1'd1;
+
+
+
+assign ce_pix = pe8M8;
+
+wire pe8M8 = ~ce[0] &  ce[1];
+wire ne8M8 = ~ce[0] & ~ce[1];
+
+wire ne4M4 = ~ce[0] & ~ce[1] & ~ce[2];
+
+wire pe2M2 = ~ce[0] & ~ce[1] & ~ce[2] &  ce[3];
+wire ne2M2 = ~ce[0] & ~ce[1] & ~ce[2] & ~ce[3];
+
+wire pe1M1 = ~ce[0] & ~ce[1] & ~ce[2] & ~ce[3] &  ce[4];
+
+//-------------------------------------------------------------------------------------------------
+
+wire ioF8 = !(!iorq && a[7:0] == 8'hF8); // psg addr
+wire ioF9 = !(!iorq && a[7:0] == 8'hF9); // psg data
+
+wire ioFA = !(!iorq && a[7:0] == 8'hFA); // crtc addr
+wire ioFB = !(!iorq && a[7:0] == 8'hFB); // crtc data
+
+wire ioFF = !(!iorq && a[7:0] == 8'hFF);
+
+//-------------------------------------------------------------------------------------------------
+
+wire reset = power & kreset;
+
+assign led = reset;
+wire[ 7:0] d;
+wire[ 7:0] q;
+wire[15:0] a;
+
+wire rfsh, mreq, iorq, rd, wr;
+
+cpu Cpu
+(
+	.clock  (clock  ),
+	.cep    (pe2M2  ),
+	.cen    (ne2M2  ),
+	.reset  (reset  ),
+	.rfsh   (rfsh   ),
+	.mreq   (mreq   ),
+	.iorq   (iorq   ),
+	.rd     (rd     ),
+	.wr     (wr     ),
+	.nmi    (nmi    ),
+	.d      (d      ),
+	.q      (q      ),
+	.a      (a      )
+);
+
+//-------------------------------------------------------------------------------------------------
+
+wire crtcCs = !(!ioFA || !ioFB);
+wire crtcRs = a[0];
+wire crtcRw = wr;
+
+wire[ 7:0] crtcQ;
+
+wire[13:0] crtcMa;
+wire[ 4:0] crtcRa;
+
+wire cursor;
+
+UM6845R Crtc
+(
+	.TYPE   (1'b0   ),
+	.CLOCK  (clock  ),
+	.CLKEN  (pe1M1  ),
+	.nRESET (reset  ),
+	.ENABLE (1'b1   ),
+	.nCS    (crtcCs ),
+	.R_nW   (crtcRw ),
+	.RS     (crtcRs ),
+	.DI     (q      ),
+	.DO     (crtcQ  ),
+	.VSYNC  (vsync  ),
+	.HSYNC  (hsync  ),
+`ifdef USE_BLANK
+	.HBLANK (hblank ),
+	.VBLANK (vblank ),
+`endif	
+
+	.DE     (crtcDe ),
+	.FIELD  (       ),
+	.CURSOR (cursor ),
+	.MA     (crtcMa ),
+	.RA     (crtcRa )
+);
+
+reg[1:0] cur;
+always @(posedge clock) if(pe1M1) cur <= { cur[0], cursor };
+
+//-------------------------------------------------------------------------------------------------
+
+wire bdir = (!wr && !ioF8) || (!wr && !ioF9);
+wire bc1  = (!wr && !ioF8) || (!rd && !ioF9);
+
+wire[7:0] psgA;
+wire[7:0] psgB;
+wire[7:0] psgC;
+wire[7:0] psgQ;
+
+jt49_bus Psg
+(
+	.clk    (clock  ),
+	.clk_en (pe2M2  ),
+	.rst_n  (reset  ),
+	.bdir   (bdir   ),
+	.bc1    (bc1    ),
+	.din    (q      ),
+	.dout   (psgQ   ),
+	.A      (psgA   ),
+	.B      (psgB   ),
+	.C      (psgC   ),
+	.sel    (1'b0   )
+);
+
+
+//-------------------------------------------------------------------------------------------------
+
+
+
+wire[9:0] dacD = { 2'b00, psgA } + { 2'b00, psgB } + { 2'b00, psgC };
+
+`ifdef USE_DAC
+dac #(.MSBI(9)) Dac
+(
+	.clock  (clock  ),
+	.reset  (reset  ),
+	.d      (dacD   ),
+	.q      (sound  )
+);
+`else
+ assign audio_l = {dacD,6'b0};
+ assign audio_r = audio_l;
+`endif
+
+//-------------------------------------------------------------------------------------------------
+
+wire[7:0] keyQ;
+wire[7:0] keyA = a[7:0];
+wire nmi,boot,kreset;
+
+
+`ifdef ZX1
+keyboard Keyboard
+`else
+keyboard #(.BOOT(8'h0A), .RESET(8'h78)) Keyboard //Boot(F8) - Reset(F11)
+`endif
+(
+	.clock  (clock  ),
+	.ce     (pe8M8  ),
+	.ps2    (ps2    ),
+	.nmi    (nmi    ),
+	.boot   (boot   ),
+	.reset  (kreset ),
+	.q      (keyQ   ),
+	.a      (keyA   )
+);
+
+//-------------------------------------------------------------------------------------------------
+
+reg mode, c, b;
+always @(posedge clock) if(pe2M2) if(!ioFF && !wr) { mode, c, b } <= q[5:3];
+
+//-------------------------------------------------------------------------------------------------
+
+wire[13:0] vma = crtcMa;
+wire[ 2:0] vra = crtcRa[2:0];
+
+wire[ 7:0] memQ;
+wire ven;
+
+memory Memory
+(
+	.clock  (clock  ),
+	.hsync  (hsync  ),
+	.vcep   (pe8M8  ),
+	.vcen   (ne8M8  ),
+	.hrce   (ne4M4  ),
+	.vma    (vma    ),
+	.vra    (vra    ),
+	.b      (b      ),
+	.c      (c      ),
+	.mode   (mode   ),
+	.ven    (ven    ),
+	.color  (color  ),
+	.ce     (pe2M2  ),
+	.rfsh   (rfsh   ),
+	.mreq   (mreq   ),
+	.rd     (rd     ),
+	.wr     (wr     ),
+	.d      (q      ),
+	.q      (memQ   ),
+	.a      (a      ),
+	.keyQ   (keyQ   ),
+`ifdef ZX1 
+	.ramWe  (ramWe  ),
+	.ramDQ  (ramDQ  ),
+	.ramA   (ramA   )
+`elsif USE_BRAM 
+   .filler (       )	
+`elsif USE_SDRAM
+	.ramCk  (ramCk  ),
+	.ramCe  (ramCe  ),
+	.ramCs  (ramCs  ),
+	.ramWe  (ramWe  ),
+	.ramRas (ramRas ),
+	.ramCas (ramCas ),
+	.ramDqm (ramDqm ),
+	.ramDQ  (ramDQ  ),
+	.ramBA  (ramBA  ),
+	.ramA   (ramA   )
+`endif
+);
+
+assign pixel = (ven || cur[1]) && crtcDe;
+
+//-------------------------------------------------------------------------------------------------
+
+assign d
+	= !mreq ? memQ
+	: !ioF9 ? psgQ
+	: !ioFB ? crtcQ
+	: !ioFF ? { 7'd0, tape }
+	: 8'hFF;
+
+
+//-------------------------------------------------------------------------------------------------
+
+reg [23:0]       io_ram_addr;
+wire             iorrd;
+reg              iorrd_r;
+    
+reg [2:0]        tapebits;		// motor on/off, plus two bits for output signal level
+`define tapemotor tapebits[2]
+    
+reg              taperead;		// only when motor is on, 0 = write, 1 = read
+reg [11:0]       tape_cyccnt;		// CPU cycle counter for cassette carrier signal
+//signal tape_leadin	: std_logic_vector(7 downto 0);		-- additional 128 bits for sync-up, just in case
+integer          tape_bitptr;
+    
+reg              tapebit_val;		// represents bit being sent from cassette file
+reg              tapelatch;		// represents input bit from cassette (after signal conditioning)
+//signal tapelatch_resetcnt	: std_logic_vector(3 downto 0) $ "0000";	-- when port is read, reset value - but only after a few cycles
+    
+    wire [15:0]      cpua;
+    wire [7:0]       cpudo;
+    wire [7:0]       cpudi;
+    wire             cpuwr;
+    wire             cpurd;
+    wire             cpumreq;
+    wire             cpuiorq;
+    wire             cpum1;
+    reg              cpuclk;
+    reg              cpuclk_r;
+    reg              clk_25ms;
+
+    wire             ior;
+    wire             iow;
+    reg              widemode;
+
+    wire [16:0]      ram_a_addr;
+    wire [16:0]      ram_b_addr;
+    wire [7:0]       ram_a_dout;
+    wire [7:0]       ram_b_dout;
+
+    always @(posedge clock or posedge dn_go or negedge loader_download or posedge reset)
+        if ((dn_go == 1'b1 & loader_download == 1'b0) | reset == 1'b1)
+        begin
+            io_ram_addr <= 24'h010000;		// above 64k
+            iorrd_r <= 1'b0;
+            
+            tapebits <= 3'b000;
+            tape_cyccnt <= 12'h000;
+            //		tape_leadin <= x"00";
+            tape_bitptr <= 7;
+            tapelatch <= 1'b0;
+        end
+        else
+            //		tapelatch_resetcnt <="0000";
+            
+            
+            begin
+                cpuclk_r <= cpuclk;
+                
+                if ((cpuclk_r != cpuclk) & cpuclk == 1'b1)
+                begin
+                    
+                    //----  Extended memory 'hack' (covers ports 4/5/6) ------
+                    //
+                    // Note:
+                    // The original MiSTer port of HT1080Z placed cassette data at memory address 0x10000,
+                    // beyond accessibility of the CPU.  It created port-based access to this data
+                    // **WHICH NEVER EXISTED ON THE ORIGINAL MACHINE**
+                    // ...in order to speed up data transfer from the cassette (normally 500 baud).
+                    //
+                    // To use this, it required a hacked version of the boot ROM, accessing these ports
+                    // ports instead of the original cassette data.
+                    
+                    if (iow == 1'b0 & cpua[7:2] == 6'b000001)		// write to port 4 5 6
+                        case (cpua[1:0])
+                            2'b00 :		// sets address of memory-read pointer
+                                io_ram_addr[7:0] <= cpudo;
+                            2'b01 :
+                                io_ram_addr[15:8] <= cpudo;
+                            2'b10 :
+                                io_ram_addr[23:16] <= cpudo;
+                            default :
+                                ;
+                        endcase
+                    
+                    iorrd_r <= iorrd;
+                    if (iorrd == 1'b0 & iorrd_r == 1'b1 & cpua[7:2] == 6'b000001)		// read from port 4 reads memory directly
+                        io_ram_addr <= io_ram_addr + 1;
+                    
+                    //----  Cassette data I/O (covers port $FF) ------
+                    //
+                    // Added in order to support regular/original BIOS ROMs.
+                    // Synthesizes the cassette data from .CAS files; doesn't yet accept audio files as input.
+                    // Since loading a 13KB fie takes several minutes at regular speed, this version automatically
+                    // sets CPU to top speed on input.
+                    //
+                    if (iow == 1'b0 & cpua[7:0] == 8'hff)		// write to tape port
+                    begin
+                        
+                        if ((`tapemotor == 1'b0) & (cpudo[2] == 1'b1))		// if start motor, then reset pointer
+                        begin
+                            io_ram_addr <= 24'h010000;
+                            tape_bitptr <= 7;
+                            taperead <= 1'b0;
+                        end
+                        
+                        else if ((`tapemotor == 1'b1) & (cpudo[2] == 1'b0))		// if stop motor, then reset tape read status
+                            taperead <= 1'b0;
+                        
+                        tapebits <= cpudo[2:0];
+                        widemode <= cpudo[3];
+                        tapelatch <= 1'b0;		// tapelatch is set by cassette data bit, and only reset by write to port $FF
+                    end
+                    
+                    if (ior == 1'b0 & cpua[7:0] == 8'hff)
+                    begin
+                        if (`tapemotor == 1'b1 & taperead == 1'b0)		// reading the port while motor is on implies tape playback
+                        begin
+                            taperead <= 1'b1;
+                            tape_cyccnt <= 12'h000;
+                        end
+                    end
+                    //						tape_leadin <= x"00";
+                    
+                    if (taperead == 1'b1)
+                    begin
+                        tape_cyccnt <= tape_cyccnt + 1;		// count in *CPU* cycles, regardless of clock speed
+                        
+                        if (tape_cyccnt < 12'h200)		// fixed-timing sync clock bit - hold the signal high for a bit
+                            tapelatch <= 1'b1;		// DO NOT reset the latch until port is read
+                        // uncomment the following line when debugging cassette input:
+                        //tapebits(1 downto 0) <= "01";	-- ** remove when working
+                        
+                        if (tape_cyccnt == 12'h6ff)		// after 1791 cycles (~1ms @ normal clk), actual data bit is written only if it's a '1'
+                        begin
+                            // timing reverse-engineered from Level II ROM cassette write routine
+                            
+                            tapebit_val <= ram_a_dout[tape_bitptr];
+                            
+                            // uncomment the following lines when debugging cassette input:
+                            //if ram_a_dout(tape_bitptr) = '1' then		-- ** make a noise
+                            //	tapebits(1 downto 0) <= "01";				-- ** remove when working
+                            //end if;												-- **
+                            
+                            if (tape_bitptr == 0)
+                            begin
+                                io_ram_addr <= io_ram_addr + 1;
+                                tape_bitptr <= 7;
+                            end
+                            else
+                                tape_bitptr <= tape_bitptr - 1;
+                        end
+                        
+                        if (tape_cyccnt > 12'h6ff & tape_cyccnt < 12'h8ff)
+                        begin
+                            
+                            if (tapebit_val == 1'b1)		// if set, hold it for 200 cycles like a real tape
+                                tapelatch <= 1'b1;		// DO NOT reset the latch if '0'
+                        end
+                        // uncomment the following line when debugging cassette input:
+                        //tapebits(1 downto 0) <= "01";			-- ** make a noise  ** remove when working
+                        
+                        if (tape_cyccnt >= 12'he08)		// after 3582 cycles (~2ms), sync signal is written (and cycle reset)
+                            tape_cyccnt <= 12'h000;
+                    end
+                end
+            end
+
+//-------------------------------------------------------------------------------------------------
+endmodule
+//-------------------------------------------------------------------------------------------------
